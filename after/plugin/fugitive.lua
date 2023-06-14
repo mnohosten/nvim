67ab58f70c618892181bdd76f49a80c4de330ec1
Martin Krizan <mnohosten@gmail.com>
2023-06-14 17:14:19 +0200

    Initial commmit

---
+++ after/plugin/fugitive.lua
@@ -0,0 +1,7 @@
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiff)
+vim.keymap.set("n", "<leader>gb", vim.cmd.Gblame)
+vim.keymap.set("n", "<leader>gc", vim.cmd.Gcommit)
+vim.keymap.set("n", "<leader>gp", vim.cmd.Gpush)
+vim.keymap.set("n", "<leader>gl", vim.cmd.Gpull)
+
---
+++ lua/krizos/init.lua
@@ -0,0 +1,2 @@
+require("krizos.set")
+require("krizos.remap")
