3a0ed0c7ad348af952d31a3333f36a0c5dbad04a
Martin Krizan <mnohosten@gmail.com>
2024-06-23 23:11:52 +0200

    FIX: Installation of neotest

--- lua/krizos/packer.lua
+++ lua/krizos/packer.lua
@@ -82,6 +82,7 @@ return require('packer').startup(function(use)
     use {
         "nvim-neotest/neotest",
         requires = {
+            "nvim-neotest/nvim-nio",
             "nvim-lua/plenary.nvim",
             "nvim-treesitter/nvim-treesitter",
             "antoinemadec/FixCursorHold.nvim",
